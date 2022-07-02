7785ff350dbba553a6e24c7882be06efd18db075
Baibhav <paudelbaibhav0@gmail.com>
2022-07-02 23:17:21 +0545

    2022/7/02

---
+++ Activity classes/Java Programming Practice Exercise 2/Extract.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Extract {
+    public static float price;
+    public static int dollars, cent;
+
+    public Extract() {
+        dollars = (int)(price);
+        cent = (int)((price - dollars) * 100 + 0.5);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your price :");
+        price = scan.nextFloat();
+        scan.close();
+
+        Extract ex = new Extract();
+        System.out.println(Extract.dollars + " Dollars, " + Extract.cent + " cents ");
+    }
+}
