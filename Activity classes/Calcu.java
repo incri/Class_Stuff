877192ccb5561a68915ae9a6675cacbf881ed4ca
Baibhav <paudelbaibhav0@gmail.com>
2022-07-14 08:57:16 +0545

    2022/7/14

---
+++ Activity classes/Calcu.java
@@ -0,0 +1,10 @@
+public class Calcu{
+    int[] a;
+    int sum;
+    public void calc(){
+        a = new int[10];
+        for (int i = 0; i < 20; i++){
+            sum += a[i];
+        }
+    }
+}
