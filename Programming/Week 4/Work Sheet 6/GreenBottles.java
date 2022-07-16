a2f5f3e7a6c598b21303aa531edd6dd7a44a644d
Baibhav <paudelbaibhav0@gmail.com>
2022-07-16 16:09:16 +0545

    2022/7/16

--- Programming/Week 4/Work Sheet 6/GreenBottles.java
+++ Programming/Week 4/Work Sheet 6/GreenBottles.java
@@ -1,8 +1,30 @@
+//importing scanner pakage
 import java.util.Scanner;
 
 public class GreenBottles{
+    //needed variables
+    public static int x;
+    public static int y;
     public void tenGreenBottles(){
+        //for loop where loop run from 10 to 0
         for (int i=10; i>0; i--){
+            //printing according to question
+            System.out.println(i+"green bottles standing on the wall,");
+            System.out.println(i+"green bottles standing on the wall,");
+            System.out.println("And if 1 green bottle should accidently fall,");
+            //if condition to differ print of last line according to question
+            if(i>1){
+                System.out.println("There’ll be "+ (i-1) + " green bottles standing on the wall.\n\n");
+            }
+            else{
+                System.out.println("There’ll be no green bottles standing on the wall.");
+            }
+        }
+    }
+    public void xGreenBottles(){
+        //method where we can give the starting point of the poem
+        for (int i=x; i>0; i--){
+            //the starting point is x that is input by the user
             System.out.println(i+"green bottles standing on the wall,");
             System.out.println(i+"green bottles standing on the wall,");
             System.out.println("And if 1 green bottle should accidently fall,");
@@ -13,13 +35,33 @@ public class GreenBottles{
                 System.out.println("There’ll be no green bottles standing on the wall.");
             }
         }
-    }public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the total number of green bottles: ");
-        int n = scanner.nextInt();
-        scanner.close();
+    }
+    public void xyGreenBottles(){
+        //method where we can give the starting point as well as the finish point of the poem
+        for (int i=x; i>y; i--){
+            //the starting point is x and the ending point is y that is input by the user
+            System.out.println(i+"green bottles standing on the wall,");
+            System.out.println(i+"green bottles standing on the wall,");
+            System.out.println("And if 1 green bottle should accidently fall,");
+            if(i>1){
+                System.out.println("There’ll be "+ (i-1) + " green bottles standing on the wall.\n\n");
+            }
+            else{
+                System.out.println("There’ll be no green bottles standing on the wall.");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        //main function with scanner instantiation and message for user input
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter you X number: ");
+        x = scan.nextInt();
+        System.out.println("Enter you Y number: ");
+        y = scan.nextInt();
+        scan.close();
+        //calling the xyGreenBottle function as it is enough output for now
         GreenBottles gb = new GreenBottles();
-        gb.tenGreenBottles();
+        gb.xyGreenBottles();
     }
 
 }
--- Programming/Week 4/Work Sheet 6/Sum.java
+++ Programming/Week 4/Work Sheet 6/Sum.java
@@ -1,26 +1,27 @@
-import java.util.Scanner;
 
-public class Sum {
+import java.util.Scanner;//importing scanner pakage
+
+public class Sum {//needed variable
     public static int n;
     public static int sum;
 
-    public Sum(){
+    public Sum(){//constructor
         sum=0;
     }
 
-    public void addition(){
-        if (n<0){
+    public void addition(){//function to calculate the required result
+        if (n<0){//first condition where input value is negative
             System.out.println("Invlid data, Number can't be negative");
         }
-        else if (n==0){
+        else if (n==0){//secounf condition where input value is 0
             System.out.println("sum = 0");
         }
-        else if (n==1){
+        else if (n==1){//third condition where input value is 1
             System.out.println("sum = 1");
         }
-        else{
-            for (int i = 0; i <= n; i++){
-                sum = sum + i;
+        else{//fourth condition where input value is valid above 1
+            for (int i = 0; i <= n; i++){//loop that run 0 to given input number
+                sum = sum + i;//every looped number upto n is added 
             }
             System.out.println("addition upto "+ n + " = "+ sum);
         }
@@ -28,6 +29,7 @@ public class Sum {
         
     }
     public static void main(String[] args) {
+        //initializing Scanner and calling method
         Scanner scan = new Scanner(System.in);
         System.out.println("enter your data : ");
         n = scan.nextInt();
